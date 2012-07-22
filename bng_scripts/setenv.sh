0f9c066bc4322b0d9da042493509a714c6f323b0
Rory Donovan <donovanr@alumni.reed.edu>
2012-07-22 13:26:35 -0400

    added setenv.sh

---
+++ bng_scripts/setenv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# BioNetGen Path
+BNG_PATH=~/Dropbox/CPCB_Classes/cell_and_systems/cell_and_systems_project/BioNetGen-2.2.0-stable
+  #BNG_PATH=/Users/ajsedgewick/BioNetGen-2.2.0-stable
+
+# Set which run_network executable to use -- system dependent
+RUN_NETWORK=run_network_i386-darwin
+  #RUN_NETWORK=run_network_i686-linux
+  #RUN_NETWORK=run_network_x86_64-linux
+  #RUN_NETWORK=run_network_MSWin32.exe
+
