daa9efaaf014a37243ff1eee2f8406e3848ff0b4
Rory Donovan <donovanr@alumni.reed.edu>
2012-07-22 13:23:44 -0400

    fixed status.sh tool

--- tools/status.sh
+++ tools/status.sh
@@ -1,1 +1,13 @@
-sed -n 'H; /^snapshot/h; ${g;p;}' snapshot | awk 'NR%100==1'
+#!/bin/bash
+
+# usage: run from directory where your in-progress modelname.snapshot file is.
+# syntax: ./tools/status modelname.snapshot
+
+MODELNAME=$(basename "$1" .snapshot)
+
+N_ALL_PAR=$(grep nallpar input/${MODELNAME}.input | awk '{print $2}')
+N_BIN=$(grep nbin input/${MODELNAME}.input | awk '{print $2}')
+N_PAR=$(($N_ALL_PAR / $N_BIN))
+
+# print the snapshot number and the weight of the 4th particle in each bin in the last snapshot
+sed -n 'H; /^snapshot/h; ${g;p;}' ${MODELNAME}.snapshot | awk -v Npar=$N_PAR 'NR%Npar==1'
