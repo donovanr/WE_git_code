8a0b8047dcf0b1f27dd4dd1d088e554aa5c8030c
Rory Donovan <donovanr@alumni.reed.edu>
2012-07-24 14:27:32 -0400

    tweaked status.sh

--- tools/status.sh
+++ tools/status.sh
@@ -9,5 +9,5 @@ N_ALL_PAR=$(grep nallpar input/${MODELNAME}.input | awk '{print $2}')
 N_BIN=$(grep nbin input/${MODELNAME}.input | awk '{print $2}')
 N_PAR=$(($N_ALL_PAR / $N_BIN))
 
-# print the snapshot number and the weight of the 4th particle in each bin in the last snapshot
+# print the snapshot number and the weight of a particle in each bin in the most recently completed snapshot
 sed -n 'H; /^snapshot/h; ${g;p;}' ${MODELNAME}.snapshot | awk -v Npar=$N_PAR 'NR%Npar==1'
